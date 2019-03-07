0347504e7b9005dc52bc8918f40a5603226d7e2c
Miguel Castrillon <miguel.chory@hotmail.com>
2019-03-07 14:53:36 -0500

    Persistor, fix when persistor data is loading Table styles fix

--- src/index.js
+++ src/index.js
@@ -3,19 +3,25 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { LocaleProvider } from 'antd';
+import { getPersistor } from '@rematch/persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import './styles/index.css';
 import Layout from './pages/Layout';
 import store from './config/store';
 import getLocalesAntd from './config/locale';
 import * as registerServiceWorker from './registerServiceWorker';
 
+const persistor = getPersistor();
+
 const App = () => {
   return (
     <LocaleProvider locale={getLocalesAntd()}>
       <Provider store={store}>
-        <Router>
-          <Layout />
-        </Router>
+        <PersistGate persistor={persistor}>
+          <Router>
+            <Layout />
+          </Router>
+        </PersistGate>
       </Provider>
     </LocaleProvider>
   );
