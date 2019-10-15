fabcfa1689709fb1464c03f99437309a79bfa06b
Miguel Cast <miguel2@outlook.com>
2019-10-15 07:26:22 -0500

    Upgrade yarns locks Updates Locales with Ant Design

--- packages/web/src/index.js
+++ packages/web/src/index.js
@@ -2,7 +2,7 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { LocaleProvider, Spin } from 'antd';
+import { ConfigProvider, Spin } from 'antd';
 import { getPersistor } from '@rematch/persist';
 import { PersistGate } from 'redux-persist/integration/react';
 import { useTranslation } from 'react-i18next';
@@ -26,7 +26,7 @@ const AppSuspense = () => (
 const App = () => {
   const { i18n } = useTranslation();
   return (
-    <LocaleProvider locale={getLocalesAntd(i18n.language)}>
+    <ConfigProvider locale={getLocalesAntd(i18n.language)}>
       <ProviderEasyCrud client={client} type='rest'>
         <Provider store={store}>
           <PersistGate persistor={persistor}>
@@ -36,7 +36,7 @@ const App = () => {
           </PersistGate>
         </Provider>
       </ProviderEasyCrud>
-    </LocaleProvider>
+    </ConfigProvider>
   );
 };
 
